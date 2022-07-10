b75bcbc8e3da61e1d58d153a08ac0d7b69573651
kesel <a.kiselyoff@gmail.com>
2022-07-10 16:22:54 +0300

    createAction example

--- src/redux/counter/counter-actions.js
+++ src/redux/counter/counter-actions.js
@@ -1,11 +1,16 @@
+import { createAction } from '@reduxjs/toolkit';
 import actionTypes from './counter-types';
 
-export const increment = value => ({
-  type: actionTypes.INCREMENT,
-  payload: value,
-});
+export const increment = createAction(actionTypes.INCREMENT); //передаем аргументом то что было в type в старом виде, payload передаем через вызов increment(...)
 
-export const decrement = value => ({
-  type: actionTypes.DECREMENT,
-  payload: value,
-});
+// export const increment = value => ({
+//   type: actionTypes.INCREMENT,
+//   payload: value,
+// });
+
+export const decrement = createAction(actionTypes.DECREMENT); // для вложенных обьектов используем второй параметр createAction(type, prepareAction)
+
+// export const decrement = value => ({
+//   type: actionTypes.DECREMENT,
+//   payload: value,
+// });
