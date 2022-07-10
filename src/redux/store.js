407555021392aa861483412b5334cd0499a22b8a
kesel <a.kiselyoff@gmail.com>
2022-07-10 16:11:23 +0300

    configureStore exemple

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,25 @@
-import { createStore, combineReducers } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';//не нужен делается под капотом
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import counterReducer from './counter/counter-reducer';
 
-const rootReducer = combineReducers({
-  counter: counterReducer,
-});
+// const rootReducer = combineReducers({// перенесли готовый объект в configureStore
+//   counter: counterReducer,
+// });
 
-const store = createStore(rootReducer, composeWithDevTools());
+// const store = createStore(rootReducer, composeWithDevTools());//было
+
+const middleware = [...getDefaultMiddleware(), logger]; //прослойка между тем как action dispach и попаданием в reducer. logger для выведения в консоли инфо
+
+const store = configureStore({
+  //под капотом обернет обьект reducer в combineReducers и сделает rootRducer
+  reducer: {
+    counter: counterReducer,
+  },
+  middleware,
+
+  devTools: process.env.NODE_ENV === 'development', // production вместо development скроет devTools в браузере
+}); //стало
 
 export default store;
 
